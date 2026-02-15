e07c03b0c22fcf1fa43169918f96766868a0668f
Warren Hoburg <whoburg@alum.mit.edu>
2026-02-15 11:50:20 -0600

    drop deprecated constant

--- gpkitmodels/__init__.py
+++ gpkitmodels/__init__.py
@@ -6,6 +6,4 @@ from gpkit import Variable
 
 from .tools.modular import PerformanceModel, PhysicalComponent
 
-g = Variable(
-    "g", 9.81, "m/s^2", "earth surface gravitational acceleration", constant=True
-)
+g = Variable("g", 9.81, "m/s^2", "earth surface gravitational acceleration")
