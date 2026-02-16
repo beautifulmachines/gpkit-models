dd35c93a5c3c525be20af5789667252f8c09d559
Warren Hoburg <whoburg@alum.mit.edu>
2026-02-16 12:15:30 -0600

    bump version

--- gpkitmodels/__init__.py
+++ gpkitmodels/__init__.py
@@ -1,9 +1,11 @@
 """GPkit Models - Library of exponential cone compatible sizing models"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from gpkit import Variable
 
 from .tools.modular import PerformanceModel, PhysicalComponent
 
 g = Variable("g", 9.81, "m/s^2", "earth surface gravitational acceleration")
+
+__all__ = (PerformanceModel, PhysicalComponent)
